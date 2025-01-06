0e94405dd07a6007d180eaed98a9f2b9acd85501
eno-mush <166697444+eno-mush@users.noreply.github.com>
2025-01-06 13:17:10 +0900

    Update todo app name

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -3,6 +3,11 @@ function App() {
     return (
         <Container>
             <Row>
+                <Col>
+                    <h1>ToDo App by Sachiko Enomoto</h1>
+                </Col>
+            </Row>
+            <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
                 </Col>
